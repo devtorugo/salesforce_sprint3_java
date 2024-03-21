f549690fd659b4e6e4377cd9047a64673422df5f
Victor Hugo <victorzarbesu@gmail.com>
2024-03-21 15:36:55 -0300

    Implementando Licenca_SoftwareRepository

---
+++ src/main/java/fiap/challenge/sprin3/repositories/Licenca_SoftwareRepository.java
@@ -0,0 +1,7 @@
+package fiap.challenge.sprin3.repositories;
+
+
+import fiap.challenge.sprin3.entities.Licenca_Software;
+
+public class Licenca_SoftwareRepository extends _BaseRepositoryImpl<Licenca_Software> {
+}
