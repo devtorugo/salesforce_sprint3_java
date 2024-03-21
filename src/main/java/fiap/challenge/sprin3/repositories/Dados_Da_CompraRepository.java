bd1fe36526a16d638063ec462e40ab3c688f363d
Victor Hugo <victorzarbesu@gmail.com>
2024-03-21 15:30:57 -0300

    Implementando Dados_Da_CompraRepository

---
+++ src/main/java/fiap/challenge/sprin3/repositories/Dados_Da_CompraRepository.java
@@ -0,0 +1,7 @@
+package fiap.challenge.sprin3.repositories;
+
+
+import fiap.challenge.sprin3.entities.Dados_Da_Compra;
+
+public class Dados_Da_CompraRepository extends  _BaseRepositoryImpl<Dados_Da_Compra> {
+}
