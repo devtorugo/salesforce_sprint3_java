332a6959177ad3043534b663f5092adc2386fb35
Victor Hugo <victorzarbesu@gmail.com>
2024-03-21 15:35:37 -0300

    Implementando Informacoes_Do_ServicoRepository

---
+++ src/main/java/fiap/challenge/sprin3/repositories/Informacoes_Do_Servico_Repository.java
@@ -0,0 +1,7 @@
+package fiap.challenge.sprin3.repositories;
+
+
+import fiap.challenge.sprin3.entities.Informacoes_Do_Servico;
+
+public class Informacoes_Do_Servico_Repository extends _BaseRepositoryImpl<Informacoes_Do_Servico> {
+}
