794ee17a208d01aa20b8f207ca6175df36478bd9
Victor Hugo <victorzarbesu@gmail.com>
2024-03-21 13:12:36 -0300

    Criando BaseEntity

---
+++ src/main/java/fiap/challenge/sprin3/entities/_BaseEntity.java
@@ -0,0 +1,30 @@
+package fiap.challenge.sprin3.entities;
+
+
+public abstract class _BaseEntity {
+    private int id;
+
+    public _BaseEntity(){
+
+    }
+
+    public _BaseEntity(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "_BaseEntity{" +
+                "id=" + id +
+                '}';
+    }
+}
+
